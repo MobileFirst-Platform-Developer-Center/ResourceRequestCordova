209ddf9d6f6a87e73d7e1e9bc8546c68d51cac09
Leandro David <ldavid@br.ibm.com>
2016-01-19 19:59:28 -0200

    update to use JavaAdapter - there is a problem with the Post request - A defect was opened for it

--- www/js/index.js
+++ www/js/index.js
@@ -1,19 +1,3 @@
-/**
-* Copyright 2015 IBM Corp.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 var Messages = {
     // Add here your messages for the default language.
     // Generate a similar file with a language suffix containing the translated messages.
@@ -25,70 +9,60 @@ var wlInitOptions = {
     // For initialization options please refer to IBM MobileFirst Platform Foundation Knowledge Center.
 };
 
-var busyIndicator ;
-
 // Called automatically after MFP framework initialization by WL.Client.init(wlInitOptions).
 function wlCommonInit(){
-  busyIndicator = new WL.BusyIndicator();
-  app.loadFeeds();
+	// Common initialization code goes here
+  WL.Logger.info("at wlCommonInit");
+  app.initialize();
 }
 
 var app = {
-    loadFeeds: function(){
-    	busyIndicator.show();
+    // Application Constructor
+    initialize: function() {
+      WL.Logger.info("at app.initialize");
+        this.bindEvents();
+    },
 
-    	/*
-    	 * The REST API works with all adapters and external resources, and is supported on the following hybrid environments:
-    	 * iOS, Android, Windows Phone 8, Windows 8.
-    	 * If your application supports other hybrid environments, see the tutorial for MobileFirst 6.3.
-    	 */
-    	var resourceRequest = new WLResourceRequest("/adapters/RSSReader/getFeedFiltered", WLResourceRequest.GET);
-    	resourceRequest.setQueryParameter("params", "['MobileFirst_Platform']");
-    	resourceRequest.send().then(
-    			app.loadFeedsSuccess,
-    			app.loadFeedsFailure
-    	);
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.getElementById("btn_submit").addEventListener('click', app.submitRequest);
     },
-    loadFeedsSuccess: function(result){
-    	WL.Logger.debug("Feed retrieve success");
-    	busyIndicator.hide();
-    	if (result.responseJSON.Items.length>0)
-      {
-    		app.displayFeeds(result.responseJSON.Items);
-      }
-    	else
-      {
-    		app.loadFeedsFailure();
-      }
-    },
-    loadFeedsFailure: function(result){
-    	WL.Logger.error("Feed retrieve failure");
-    	busyIndicator.hide();
-      navigator.notification.confirm(message, confirmCallback, [title], [buttonLabels])
-      WL.SimpleDialog.show("Engadget Reader", "Service not available. Try again later.",
-    			[{
-    				text : 'Reload',
-    				handler : WL.Client.reloadApp
-    			},
-    			{
-    				text: 'Close',
-    				handler : function() {}
-    			}]
-    		);
-    },
-     displayFeeds:function(items){
-    	var ul = $('#itemsList');
-    	for (var i = 0; i < items.length; i++) {
-    		var li = $('<li/>').text(items[i].title);
-    		var pubDate = $('<div/>', {
-    			'class': 'pubDate'
-    		}).text(items[i].pubDate);
+    submitRequest:function(){
+      WL.Logger.info("at app.submitRequest");
+      var first = document.getElementById("first").value;
+      var second = document.getElementById("second").value;
+      var third = document.getElementById("third").value;
+      var age = document.getElementById("age").value;
+      var date = document.getElementById("date").value;
+      var height = document.getElementById("height").value;
 
-    		li.append(pubDate);
-    		ul.append(li);
-    	}
+      var url = "/adapters/JavaAdapter/users/"+first+"/"+second+"/"+third;
+      var resourceRequest = new WLResourceRequest(url, WLResourceRequest.POST);
+
+      resourceRequest.setQueryParameter("age", age);
+      resourceRequest.setHeader("date",date);
+
+      var formParameters ={};
+      formParameters.height=height;
+      WL.Logger.info("at app.submitRequest - sendFormParameters");
+      resourceRequest.sendFormParameters(formParameters).then(
+          app.onSuccess,
+          app.onFailure);
+    },
+    onSuccess: function(response){
+      WL.Logger.info("at onSuccess");
+      app.showResult("Success " + JSON.stringify(response.responseJSON));
+    },
+    onFailure: function(response){
+      WL.Logger.info("at onFailure");
+      app.showResult("Error " + JSON.stringify(response.responseJSON));
+    },
+    showResult:function(content){
+      var span = document.getElementById('span_result');
+      while( span.firstChild ) {
+        span.removeChild( span.firstChild );
+      }
+      span.appendChild( document.createTextNode(content) );
     }
-
 };
-
-app.initialize();
